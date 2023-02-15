b72674d9f117971208929afeb461576894d831fa
absjuniordev <abs.junnior@hotmail.com>
2023-02-15 20:14:27 -0300

    Lógica Auth #1

---
+++ lib/models/chat_user.dart
@@ -0,0 +1,13 @@
+class ChatUser {
+  final String Id;
+  final String name;
+  final String email;
+  final String imageURL;
+
+  const ChatUser({
+    required this.Id,
+    required this.name,
+    required this.email,
+    required this.imageURL,
+  });
+}
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -14,13 +14,14 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
-
-    print('AuthPage...');
-    print(formData.email);
-
-    setState(() => _isLoading = false);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
+    } catch (error) {
+      //tratar error
+    } finally {
+      setState(() => _isLoading = false);
+    }
   }
 
   @override
