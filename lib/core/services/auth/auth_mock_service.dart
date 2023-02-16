8abac8485b44866373f28b08717e7b6e763b2e65
absjuniordev <abs.junnior@hotmail.com>
2023-02-15 22:38:55 -0300

    Usuário Padrão

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -6,12 +6,21 @@ import 'package:chat/core/services/auth/auth_service.dart';
 import '../../models/chat_user.dart';
 
 class AuthMockService implements AuthService {
-  static Map<String, ChatUser> _users = {};
+  static final _defaultUser = ChatUser(
+    id: '1',
+    name: 'Teste',
+    email: 'Teste@teste.com',
+    imageURL: 'assets/images/avatar.png',
+  );
+
+  static Map<String, ChatUser> _users = {
+    _defaultUser.email: _defaultUser,
+  };
   static ChatUser? _currentUser;
   static MultiStreamController<ChatUser?>? _controller;
   static final _userStream = Stream<ChatUser?>.multi((controller) {
     _controller = controller;
-    _updateUser(null);
+    _updateUser(_defaultUser);
   });
 
   @override
