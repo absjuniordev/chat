4be5a0216c53906fa03f408fae9339fa5fa850fe
absjuniordev <abs.junnior@hotmail.com>
2023-02-16 21:29:45 -0300

    Chat Page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,7 @@
+import 'package:chat/coponents/messages.dart';
+import 'package:chat/coponents/new_messages.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -7,20 +10,47 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Chat page'),
-            TextButton(
-              onPressed: () {
-                AuthService().logout();
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Chat'),
+          actions: [
+            DropdownButton(
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Container(
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: 10),
+                        Text('Sair'),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  AuthService().logout();
+                }
               },
-              child: const Text('Logout'),
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
             ),
           ],
         ),
-      ),
-    );
+        body: SafeArea(
+          child: Column(
+            children: const [
+              Expanded(child: Messages()),
+              NewMessage(),
+            ],
+          ),
+        ));
   }
 }
