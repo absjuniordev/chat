c03c16b84e66f358d76c61ca3eca0539ced3612b
absjuniordev <abs.junnior@hotmail.com>
2023-02-17 22:26:48 -0300

    Componente Message

--- lib/core/services/chat/chat_service.dart
+++ lib/core/services/chat/chat_service.dart
@@ -3,7 +3,6 @@ import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/chat/chat_mock_service.dart';
 
 abstract class ChatService {
-  @override
   Stream<List<ChatMessage>> messagesStrem();
   Future<ChatMessage> save(String text, ChatUser user);
 
