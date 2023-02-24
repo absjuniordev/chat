90b030b297012b0de727c0f1638c6b1229d8b845
absjuniordev <abs.junnior@hotmail.com>
2023-02-23 23:03:38 -0300

    Ícone Notification

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,8 +1,12 @@
 import 'package:chat/coponents/messages.dart';
 import 'package:chat/coponents/new_messages.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/pages/notification_page.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../core/services/notification/chat_notification_service.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -14,39 +18,68 @@ class ChatPage extends StatelessWidget {
           centerTitle: true,
           title: const Text('Chat'),
           actions: [
-            DropdownButton(
-              items: [
-                DropdownMenuItem(
-                  value: 'logout',
-                  child: Container(
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.exit_to_app,
-                          color: Colors.black,
-                        ),
-                        SizedBox(width: 10),
-                        Text('Sair'),
-                      ],
+            DropdownButtonHideUnderline(
+              child: DropdownButton(
+                items: [
+                  DropdownMenuItem(
+                    value: 'logout',
+                    child: Container(
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.exit_to_app,
+                            color: Colors.black,
+                          ),
+                          SizedBox(width: 10),
+                          Text('Sair'),
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              ],
-              onChanged: (value) {
-                if (value == 'logout') {
-                  AuthService().logout();
-                }
-              },
-              icon: Icon(
-                Icons.more_vert,
-                color: Theme.of(context).primaryIconTheme.color,
+                  )
+                ],
+                onChanged: (value) {
+                  if (value == 'logout') {
+                    AuthService().logout();
+                  }
+                },
+                icon: Icon(
+                  Icons.more_vert,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
               ),
             ),
+            Stack(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (ctx) {
+                          return NotificationPage();
+                        },
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.notifications),
+                ),
+                Positioned(
+                  top: 5,
+                  right: 5,
+                  child: CircleAvatar(
+                      maxRadius: 10,
+                      backgroundColor: Colors.red,
+                      child: Text(
+                        '${Provider.of<ChatNotificationService>(context).itemsCount}',
+                        style: const TextStyle(fontSize: 12),
+                      )),
+                ),
+              ],
+            )
           ],
         ),
-        body: SafeArea(
+        body: const SafeArea(
           child: Column(
-            children: const [
+            children: [
               Expanded(child: Messages()),
               NewMessage(),
             ],
