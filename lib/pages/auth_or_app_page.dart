00463eb6fc2e9a1476393fd4dbda3cf7e8c51af1
absjuniordev <abs.junnior@hotmail.com>
2023-03-02 19:46:09 -0300

    Push Notification #01

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -5,12 +5,19 @@ import 'package:chat/pages/chat_page.dart';
 import 'package:chat/pages/loading_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+
+import '../core/services/notification/chat_notification_service.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    await Provider.of<ChatNotificationService>(
+      context,
+      listen: false,
+    ).init();
   }
 
   @override
