35bbce81d9844a49e4c982beb5a070d07929a49d
absjuniordev <abs.junnior@hotmail.com>
2023-03-02 23:17:59 -0300

    Push Notification #02

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -6,7 +6,6 @@ import 'package:chat/pages/loading_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-
 import '../core/services/notification/chat_notification_service.dart';
 
 class AuthOrAppPage extends StatelessWidget {
